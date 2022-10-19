8e0dfafc9462f6578cd0b8f8572b55d8b3b558e6
Tigran TIKSN Torosyan <personal@tiksn.com>
2022-10-19 13:50:08 +0200

    Update MetaController.cs

--- src/API.Web/Api/MetaController.cs
+++ src/API.Web/Api/MetaController.cs
@@ -1,11 +1,22 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
-using Verdant.API.Web;
 
 namespace Verdant.API.Web.Api;
 
 public class MetaController : BaseApiController
 {
+  private readonly IHttpContextAccessor _httpContextAccessor;
+  private readonly ILogger<MetaController> _logger;
+
+  public MetaController(
+    IHttpContextAccessor httpContextAccessor,
+    ILogger<MetaController> logger)
+  {
+    _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+    _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+  }
+
   /// <summary>
   /// A sample API Controller. Consider using API Endpoints (see Endpoints folder) for a more SOLID approach to building APIs
   /// https://github.com/ardalis/ApiEndpoints
@@ -17,6 +28,12 @@ public class MetaController : BaseApiController
 
     var creationDate = System.IO.File.GetCreationTime(assembly.Location);
     var version = FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
+
+    var context = _httpContextAccessor.HttpContext;
+    var activity = context.Features.Get<IHttpActivityFeature>()?.Activity;
+    activity?.SetTag("VerdantAppVersion", version);
+
+    _logger.LogInformation("Version: {AppVersion}, Last Updated: {CreationDate}", version, creationDate);
 
     return Ok($"Version: {version}, Last Updated: {creationDate}");
   }
